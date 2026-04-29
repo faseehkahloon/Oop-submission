1b87708bad60904a35995a10c7d661fc068182b0
Faseehuddin Kahloon <faseehkahloon067@gmail.com>
2026-04-29 16:03:47 +0500

    Add files via upload

---
+++ Pentagon.java
@@ -0,0 +1,12 @@
+public class Pentagon extends Shapes{
+
+    @Override
+    public void Draw(){
+        System.out.println("Pentagon is drawn");
+    }
+
+    @Override
+    public void Remove(){
+        System.out.println("Pentagon is removed");
+    }
+}
